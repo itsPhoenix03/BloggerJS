455d7b3ac34ef62e8f20314462fd3886476e675e
Shreyas <shreyas.misra03@gmail.com>
2022-10-28 15:24:13 +0530

    Changed the end point

--- src/Request.js
+++ src/Request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //BASE URL
-const BASE_URL = "https://bloggerjsserver.herokuapp.com/api";
+const BASE_URL = "https://bloggerjsserver.onrender.com/api";
 export const URL = "https://bloggerjsserver.herokuapp.com";
 
 export const Request = axios.create({
